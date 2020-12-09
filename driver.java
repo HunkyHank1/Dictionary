4f91443483f6492f06942da36266f9c5e41fd880
Henry Johnson <46233024+HunkyHank1@users.noreply.github.com>
2020-12-09 12:15:42 -0700

    I have finished testing, and added the header

--- driver.java
+++ driver.java
@@ -1,3 +1,9 @@
+/*
+Henry Johnson
+the function of this program is to test the dictionary class
+created: 12/8/20
+las edited: 12/9/20
+ */
 public class driver {
     public static void main(String[] args) {
         Dictionary dic1 = new Dictionary();
@@ -12,5 +18,15 @@ public class driver {
         dic1.put("Monkey","MMMMMMM... Monkey");
         System.out.println("should print the definition of Monkey and remove Monkey");
         System.out.println(dic1.remove("Monkey"));
+        System.out.println("should print 1");
+        System.out.println(dic1.size());
+        dic1.put("Human","Basically an overapreciated species monkey that shout words at eachother insead" +
+                " of throwing poop");
+        dic1.put("Harambe","the only monkey to ever be appreciated until he grabbed one of the human monkeys, and was " +
+                "shot by another human monkey");
+        System.out.println("should print the collection containing all of the words in the dictionary of monkeys");
+        System.out.println(dic1.keys());
+        System.out.println("should print the collection containing all of the definitions of the monkeys");
+        System.out.println(dic1.values());
     }
 }
